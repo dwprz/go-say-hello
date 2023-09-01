371270c9ddfd115818d5bde836e8461187bf09ef
dwprz <dwiprasetiyo223@gamil.com>
2023-09-01 16:04:37 +0700

    Update SayHello

--- go-say-hello.go
+++ go-say-hello.go
@@ -2,5 +2,5 @@ package go_say_hello
 
 // *jangan lupa nama depan functionnya huruf besar supaya bisa di set sebagai publik
 func SayHello() string{
-	return "Hello"
+	return "Hello World"
 }
--- say-hello.go
+++
@@ -1,6 +0,0 @@
-package gosayhello
-
-// *jangan lupa nama depan functionnya huruf besar supaya bisa di set sebagai publik
-func SayHello() string{
-	return "Hello"
-}
